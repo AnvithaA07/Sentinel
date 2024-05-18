666efdb6a113cb93ed5f45b932338ae2d710c270
Saakshi-Shenoy <shenoy.saakshi910@gmail.com>
2024-05-18 15:28:37 +0530

    basic layout

---
+++ src/components/navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+function navbar(props) {
+  return (
+    <header className="text-gray-400 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a href className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className={`w-10 h-10 text-white p-2 bg-${props.theme}-500 rounded-full`}
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+          </svg>
+          <span className="ml-3 text-xl">Sentenial</span>
+        </a>
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <a href="/" className="mr-5 hover:text-white">HOME</a>
+          <a href="/scan" className="mr-5 hover:text-white">SCAN</a>
+        </nav>
+        <button className="inline-flex items-center border-0 py-1 px-3 focus:outline-none rounded text-black mt-4 md:mt-0">
+          Button
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7" />
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
+}
+
+
+export default navbar;
